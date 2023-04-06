e0a523c4562031eeb2e24d05001b6b66efffd579
ajayvamsee24 <ajay.vamsee@techolution.com>
2023-04-06 18:15:29 +0530

    chat gpt web view with google sign integration

---
+++ app/src/main/java/com/example/chatgptwebview/MainActivity.java
@@ -0,0 +1,180 @@
+package com.example.chatgptwebview;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+    private String userAgent;
+
+    private WebView webViewPopUp;
+    private AlertDialog builder;
+    private Context globalContext;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        if (!webSettings.getJavaScriptEnabled()) {
+            // Show a dialog prompting the user to enable JavaScript
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Please turn on JavaScript and reload the page.")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", (dialog, id) -> {
+                        // Reload the page
+                        myWebView.reload();
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        } else {
+            myWebView.loadUrl("https://chat.openai.com");
+        }
+
+
+        // Set User Agent
+        userAgent = System.getProperty("http.agent");
+        webSettings.setUserAgentString(userAgent + "yourAppName");
+
+        // Enable Cookies
+        CookieManager.getInstance().setAcceptCookie(true);
+        if(android.os.Build.VERSION.SDK_INT >= 21)
+            CookieManager.getInstance().setAcceptThirdPartyCookies(myWebView, true);
+
+        // Handle Popups
+        myWebView.setWebChromeClient(new CustomChromeClient());
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setSupportMultipleWindows(true);
+        globalContext = this.getApplicationContext();
+
+        // WebView Tweaks
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        //webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setSaveFormData(true);
+        webSettings.setEnableSmoothTransition(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+
+
+        myWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                //findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
+
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                // do your stuff here
+                //findViewById(R.id.progressBar).setVisibility(View.GONE);
+            }
+        });
+    }
+
+
+
+
+    class CustomChromeClient extends WebChromeClient {
+
+
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog,
+                                      boolean isUserGesture, Message resultMsg) {
+            webViewPopUp = new WebView(globalContext);
+            webViewPopUp.setVerticalScrollBarEnabled(false);
+            webViewPopUp.setHorizontalScrollBarEnabled(false);
+            webViewPopUp.setWebChromeClient(new CustomChromeClient());
+            webViewPopUp.getSettings().setJavaScriptEnabled(true);
+            webViewPopUp.getSettings().setSaveFormData(true);
+            webViewPopUp.getSettings().setEnableSmoothTransition(true);
+            webViewPopUp.getSettings().setUserAgentString(userAgent + "yourAppName");
+
+            // pop the  webview with alert dialog
+            builder = new AlertDialog.Builder(MainActivity.this).create();
+            builder.setTitle("");
+            builder.setView(webViewPopUp);
+
+            builder.setButton("Close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    webViewPopUp.destroy();
+                    dialog.dismiss();
+                }
+            });
+
+            builder.show();
+            builder.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptCookie(true);
+            if(android.os.Build.VERSION.SDK_INT >= 21) {
+                cookieManager.setAcceptThirdPartyCookies(webViewPopUp, true);
+                cookieManager.setAcceptThirdPartyCookies(myWebView, true);
+            }
+
+            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+            transport.setWebView(webViewPopUp);
+            resultMsg.sendToTarget();
+
+            return true;
+        }
+
+        @Override
+        public void onCloseWindow(WebView window) {
+            //Toast.makeText(contextPop,"onCloseWindow called",Toast.LENGTH_SHORT).show();
+            try {
+                webViewPopUp.destroy();
+            } catch (Exception e) {
+                Log.d("Destroyed with Error ", e.getStackTrace().toString());
+            }
+
+            try {
+                builder.dismiss();
+            } catch (Exception e) {
+                Log.d("Dismissed with Error: ", e.getStackTrace().toString());
+            }
+
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView!=null){
+            if (myWebView.canGoBack()){
+                myWebView.goBack();
+                return;
+            }
+        }
+        super.onBackPressed();
+    }
+}
